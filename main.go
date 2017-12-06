c3594ecf01ffa21bdbebdebe35c6c6109ab3e12b
Joscelyn James <josce.james7@hotmail.com>
2017-12-05 17:51:56 -0700

    removed gin - using gorilla mux - connected to db - no routes yet

--- main.go
+++ main.go
@@ -1,83 +1,61 @@
 package main
 
-import (
-	// "github.com/gin-gonic/gin"
-	"github.com/jinzhu/gorm"
-	_ "github.com/lib/pq"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
-	// "log"	
-	"github.com/joscelynjames/bird-app-backend/user"
-	"github.com/joscelynjames/bird-app-backend/user/login"
-	"github.com/joscelynjames/bird-app-backend/user/signup"
+import (  
+  "database/sql"
+	"fmt"
+	"net/http"
+	"github.com/gorilla/mux"
+  _ "github.com/lib/pq"
 )
 
-func main() {
-	r := gin.Default()
+const (  
+  host     = "localhost"
+  port     = 5432
+  user     = "joscelynjames"
+  dbname   = "wheres_my_pet_db"
+)
 
-	v1 := r.Group("api/v1")
-	{
-		v1.POST("/active_users", PostUser)
-		v1.GET("/users", GetUsers)
-		v1.GET("/users/:id", GetUser)
-		v1.PUT("/users/:id", UpdateUser)
-		v1.DELETE("/users/:id", DeleteUser)
-	}
+func main() {  
+	getSession()
 
-	r.Run(":8080")
+	r:= mux.NewRouter()
+
+	r.Handle("/", r)
+	r.HandleFunc("/users", getUsers).Methods("GET")
+
+	http.ListenAndServe(":8080", r)
 }
 
-func InitDb() *gorm.DB {
-	db, err := gorm.Open("postgres", "host=localhost user=joscelynjames dbname=wheres_my_pet_db sslmode=disable")
-	db.LogMode(true)
-	// Error
-	if err != nil {
-		panic(err)
-	}
-	// defer db.Close()
+func getSession() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+    "dbname=%s sslmode=disable",
+    host, port, user, dbname)
+  db, err := sql.Open("postgres", psqlInfo)
+  if err != nil {
+    panic(err)
+  }
+  defer db.Close()
 
-	db.AutoMigrate(&Active_Users{})
-	// Creating the table
-	// if !db.HasTable(&Active_Users{}) {
-	// 	db.CreateTable(&Active_Users{})
-	// 	db.Set("gorm:table_options", "ON CONFLICT").CreateTable(&Active_Users{})
-	// }
-	return db
+  err = db.Ping()
+  if err != nil {
+    panic(err)
+  }
+
+  fmt.Println("Successfully connected!")
 }
 
+type active_user struct {
+	ID         uint 	`json:"id"`
+	Email 		 string `json:"email"`
+	First_name string `json:"first_name"`
+	Last_name  string `json:"last_name"`
+	Password 	 string `json:"password"`
+	Admin			 bool   `json:"admin"`
+}
 
-func PostUser(c *gin.Context) {
-    db := InitDb()
-
-    var active_user Active_Users
-		c.Bind(&active_user)
-
-		db.Create(active_user)
-		// Display error
-		c.JSON(201, gin.H{"success": active_user})
-		
-    // if active_user.first_name != "" && active_user.last_name != "" {
-    //     db.Create(&active_user)
-    //     // Display error
-    //     c.JSON(201, gin.H{"success": active_user})
-    // } else {
-    //     // Display error
-    //     c.JSON(422, gin.H{"error": "Fields are empty"})
-    // }
+func getUsers(http.ResponseWriter, *http.Request) {
+	
 }
 
 
-func GetUsers(c *gin.Context) {
-	// code goes here
-}
 
-func GetUser(c *gin.Context) {
-	// code goes here
-}
-
-func UpdateUser(c *gin.Context) {
-	// code goes here
-}
-
-func DeleteUser(c *gin.Context) {
-	// code goes here
-}
